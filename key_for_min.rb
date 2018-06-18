912acbd47525f422ca7f099ed9327dcc8f2177d6
AyanaZaire <ayanazaire@gmail.com>
2018-06-18 22:22:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,4 @@ def key_for_min_value(name_hash)
 
 name_hash.each { |k, v| puts k if v == hash.values.max }
 
-  key_value = ""
-ikea.each do |key, value|
-  if ikea == key.least?
-    key_value = key
-  end
-end
- key_value
 end
