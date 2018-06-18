ded02189cecd86eb1b66abeb727e48e9b56f7736
AyanaZaire <ayanazaire@gmail.com>
2018-06-18 22:22:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
+hash.each { |k, v| puts k if v == hash.values.max }
 
   key_value = ""
 ikea.each do |key, value|
